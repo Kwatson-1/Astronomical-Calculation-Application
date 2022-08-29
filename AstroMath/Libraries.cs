6e30781ce6f3edc3861151b52f952df6ebc05e03
Kyle Watson <kyle.p.watson@gmail.com>
2022-08-29 14:48:10 +0800

    Initial Upload
    DLL and Driver program

---
+++ AstroMath/Libraries.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstroMath
+{
+    public class Formulas
+    {
+        // Accepts two input parameters (Observed Wavelength and Rest Wavelength) and returns a double which represents the velocity. 
+        public static double StarVelocity(double observedWavelength, double restWavelength)
+        {
+            double changeInWavelength = observedWavelength - restWavelength;
+            return 299792458 * changeInWavelength / restWavelength;
+        }
+        // Accepts a single input parameter of type double (arcsecond angle) and returns a double which is a value in parsecs.
+        public static double StarDistance(double arcsecondAngle)
+        {
+            return 1 / arcsecondAngle;
+        }
+        // Accepts an input paramter of type double (temperature in Celcius) and returns a double which is the temperature in degrees kelvin.
+        public static double TemperatureKelvin(double temperatureCelcius)
+        {
+            return temperatureCelcius + 273.15;
+        }
+        // Accepts an single input paramter of type double (black hole mass) and returns a double which is the event horizon (Schwarzschild Radius) in meters.
+        public static double EventHorizon(double blackHoleMass)
+        {
+            return 2 * (6.674 * Math.Pow(10, -11)) * blackHoleMass / (Math.Pow(299792458, 2));
+        }
+    }
+}
---
+++ AstroTest/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AstroMath;
+namespace AstroTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter 1 to calculate star velocity");
+                Console.WriteLine("Enter 2 to calculate star distance");
+                Console.WriteLine("Enter 3 to calculate temperature in kelvin");
+                Console.WriteLine("Enter 4 to calculate event horizon distance");
+                int selection = Int32.Parse(Console.ReadLine());
+                switch (selection)
+                {
+                    case 1:
+                        Console.WriteLine("Enter observed wavelength: ");
+                        double observedWavelength = Double.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter rest wavelength: ");
+                        double restWaveLength = Double.Parse(Console.ReadLine());
+                        Console.WriteLine("Star velocity is: " + AstroMath.Formulas.StarVelocity(observedWavelength, restWaveLength));
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter arcsecond angle");
+                        double arcsecondAngle = Double.Parse(Console.ReadLine());
+                        Console.WriteLine("Star distance in parsecs is: " + AstroMath.Formulas.StarDistance(arcsecondAngle));
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter temperature in celcius");
+                        double temperatureCelcius = Double.Parse(Console.ReadLine());
+                        Console.WriteLine("Temperature in kelvin is: " + AstroMath.Formulas.TemperatureKelvin(temperatureCelcius));
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter the black hole mass: ");
+                        double blackHoleMass = Double.Parse(Console.ReadLine());
+                        Console.WriteLine("The event horizon in meters is: " + AstroMath.Formulas.EventHorizon(blackHoleMass));
+                        Console.ReadLine();
+                        break;
+                }
+            }
+            
+        }
+    }
+}
