1ab86d294b63f13c2d344ff6c2a477ab670fb428
Kyle Watson <kyle.p.watson@gmail.com>
2022-10-03 00:47:45 +0800

    Update IStringReverser.cs

--- IAstroContract/IStringReverser.cs
+++ IAstroContract/IStringReverser.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using AstroMath;
 
 namespace IAstroContract
 {
-    internal interface IStringReverser
+    [ServiceContract]
+    public interface IStringReverser
     {
+        [OperationContract]
+
+
     }
 }
